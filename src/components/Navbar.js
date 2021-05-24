dff6bb258cfc713b4c5f0093ea74cd340870de15
Andres Leal <pixele9@gmail.com>
2021-05-23 20:59:16 -0700

    feat: Home page button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import Home from "../assets/Home.svg";
+import { useHistory } from "react-router-dom";
 
 export default function Navbar() {
+	const history = useHistory();
+
 	return (
-		<div className="light-bg w-60 h-screen px-6 py-8">
-			<p className="text-xl font-bold">Collections</p>	
+		<div className="light-bg w-60 h-screen px-6 py-8 cursor-pointer" onClick={() => history.push("/")}>
+			<div className="flex justify-center items-center gray-blue rounded p-2">
+				<img src={Home} />
+				<span className="ml-4">Home Page</span>
+			</div>
+			<p className="text-xl font-bold mt-8">Collections</p>	
 		</div>
 	)
 }
